2331aae66a8bac8f9f5279700f1b35060bd78876
Matthew Murray <mattm.trinsic@gmail.com>
2026-06-24 16:53:15 -0700

    Update version 1.1.1

--- PrebidMobile/Swift/Constants.swift
+++ PrebidMobile/Swift/Constants.swift
@@ -120,7 +120,7 @@ public class PrebidConstants: NSObject {
         """
     }
     
-    public static let PREBID_VERSION                                                        = "1.1.0"
+    public static let PREBID_VERSION                                                        = "1.1.1"
     public static let SDK_NAME                                                              = "life360-ads-sdk"
     
     public static let DOMAIN_KEY                                                            = "domain"
